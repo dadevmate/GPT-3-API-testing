6b69334fc8a596c133c219d81284cd5c57da2960
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-02-25 20:04:57 +0800

    Initial Commit

---
+++ GPT-3 API/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GPT-3 API
+//
+//  Created by Nethan on 25/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GPT-3 API/GPT_3_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPT_3_APIApp.swift
+//  GPT-3 API
+//
+//  Created by Nethan on 25/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GPT_3_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
