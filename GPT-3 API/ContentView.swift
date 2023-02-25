c0d3020b89e663dd893d70c645f43f5a008ed173
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-02-25 23:49:28 +0800

    first

--- GPT-3 API/ContentView.swift
+++ GPT-3 API/ContentView.swift
@@ -6,16 +6,124 @@
 //
 
 import SwiftUI
+import OpenAISwift
+import AVFoundation
+final class ViewModel: ObservableObject {
+    init() {}
+    
+    private var client: OpenAISwift?
+    
+    func setup() {
+        client = OpenAISwift(authToken: "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")
+    }
+    
+    func send(text: String, completion: @escaping (String) -> Void) {
+        client?.sendCompletion(with: text, maxTokens: 500, completionHandler: { result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case.failure:
+                break
+            }
+        })
+    }
+}
 
 struct ContentView: View {
+    
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    @State var synthesizer = AVSpeechSynthesizer()
+    @State var mic = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            VStack {
+                  Text("Please wait for a few seconds in between requests for ChatGPT to process it.")
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    List(models, id: \.self) { string in
+                        
+                        if string.contains("ChatGPT:") {
+                            Text(string)
+                            .foregroundColor(.green)
+                            .fontWeight(.light)
+                        } else {
+                            Text(string)
+                                .foregroundColor(.red)
+                                .fontWeight(.light)
+                      
+                        }
+                        
+                    }
+                    .toolbar {
+                        ToolbarItemGroup(placement: .navigationBarTrailing) {
+                            Button {
+                                mic.toggle()
+                            } label: {
+                                if mic {
+                                    Image(systemName: "speaker.wave.3.fill")
+                                   
+                                        .foregroundColor(.red)
+                                } else {
+                                    Image(systemName: "speaker.slash.fill")
+                              
+                                        .foregroundColor(.red)
+                                }
+                            }
+                        }
+                    }
+                Spacer()
+                    HStack {
+                        TextEditor(text: $text)
+                            .frame(width: 300, height: 100)
+                            .border(Color.secondary)
+                        
+                        Button {
+                            send()
+                            
+                                                      
+                        } label: {
+                            Image(systemName: "paperplane.fill")
+                                .font(.title)
+                                .foregroundColor(.green)
+                        }
+                        
+                    }
+                
+            }
+            .navigationTitle("Nethan's ChatGPT")
+        }
+    
+        
+        .onAppear {
+            viewModel.setup()
         }
         .padding()
+    }
+    func send() {
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        models.append("Me: \(text)")
+        viewModel.send(text: text) { response in
+            DispatchQueue.main.async {
+                
+                self.models.append("ChatGPT: \(response)")
+               
+                self.text = ""
+                
+                if mic {
+                    let utterance = AVSpeechUtterance(string: "\(response)")
+                    utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+                    utterance.rate = 0.5
+                    
+                    synthesizer.speak(utterance)
+                }
+            }
+        }
     }
 }
 
